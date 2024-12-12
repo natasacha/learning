f58d960ff76441fdd31eb61a9a787dc0710ede83
Natasa <tajanacha78@yahoo.com>
2024-12-12 21:05:33 +0100

    More changes

--- cypress/integration/examples/APITests.js
+++ cypress/integration/examples/APITests.js
@@ -1,37 +1,54 @@
 ///<reference types="cypress"/>
+import { faker } from '@faker-js/faker';
 
 
 
 describe('API Tests', () => {
 
+    before('create data', () => {
+       
+    })
+
     it('Create a book & delete a book', () => {
-
-        const randomIsb = Math.floor(Math.random() * 100) + 1;
-
+        const words = faker.string.alpha(10);
 
         cy.request('POST', 'http://216.10.245.166/Library/Addbook.php', {
 
             "name": "Learn Appium Automation with Java",
-            "isbn": randomIsb,
-            "aisle": "2222222",
+            "isbn": words,
+            "aisle": "33333333",
             "author": "John foe"
 
 
         }).then((response) => {
             expect(response.body).to.have.property('Msg', 'successfully added');
+            const ID = response.body.ID;
+            cy.log(ID);
             cy.request('POST', 'http://216.10.245.166/Library/DeleteBook.php', {
-                "ID" : randomIsb + "2222222"
+                
+                "ID" : ID
             }).then((response) => {
                 expect(response.body).to.have.property('msg', 'book is successfully deleted');
+
             })
+            
+ 
+               
+                
 
         })
 
-      
+        
+    
+    
 
+       
 
+       
 
+    
 
+        
     })
 
 
